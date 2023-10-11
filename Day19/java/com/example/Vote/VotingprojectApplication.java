813f22a08305c319e66f745c159aba47a4df2b75
IbrahimI100 <144885248+IbrahimI100@users.noreply.github.com>
2023-10-11 16:23:14 +0530

    Add files via upload

---
+++ Day19/java/com/example/Vote/VotingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingprojectApplication.class, args);
+	}
+
+}
